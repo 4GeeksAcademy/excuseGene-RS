5fca88fef25e7b8861fb68c2d224035595db2a0d
roseshay <165499006+roseshay@users.noreply.github.com>
2024-12-11 19:10:32 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const h1Element = document.querySelector(".text");
+  let dog = "ate";
+  dog = "peed";
+  h1Element.innerHTML = dog; //write your code here'};
 };
